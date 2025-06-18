7cfa536879e62f28f0749d99bbf77423b0a9161a
John-T-Gries <john.t.gries@gmail.com>
2025-06-17 17:04:15 -0700

    Fixed issue with video playing on mobile.

--- web.js
+++ web.js
@@ -10,11 +10,29 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 document.querySelectorAll(".project-card").forEach(card => {
     const video = card.querySelector("video");
+
     card.addEventListener("mouseenter", () => {
         video.currentTime = 0;
         video.play();
     });
     card.addEventListener("mouseleave", () => {
         video.pause();
+        video.currentTime = 0;
     });
+
+    card.addEventListener("click", () => {
+        video.currentTime = 0;
+        video.play();
+    });
+
+    card.addEventListener('mouseleave', () => {
+        const video = card.querySelector('video');
+        if (video) {
+            video.pause();
+            video.currentTime = 0;
+        }
+    });
+    
 });
+
+
